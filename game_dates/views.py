4af26c8e4a37aa5af45d854f418be457ae390c0c
Emile <bvcxzq1@gmail.com>
2023-07-23 19:14:09 +0000

    Add userUpdateSubmit View and Template - WIP

--- game_dates/views.py
+++ game_dates/views.py
@@ -80,7 +80,8 @@ def bookingSubmit(request):
                 messages.success(request, "Appointment Saved!")
                 return redirect('index')
             else:
-                messages.success(request, "The Selected Date Isn't In The Correct Time Period!")
+                messages.success(
+                    request, "The Selected Date Isn't In The Correct Time Period!")
         else:
             messages.success(request, "Please Select A Type!")
 
@@ -120,7 +121,7 @@ def userUpdate(request, id):
     # 24h if statement in template:
     delta24 = (userdatepicked).strftime('%Y-%m-%d') >= (
         today + timedelta(days=1)).strftime('%Y-%m-%d')
-    # Calling 'validWeekday' Function to Loop days you want in the next 21 days:
+    # Calling 'validWeekday' Function to Loop days you want in the next 31:
     validateWeekdays = validWeekday(31)
 
     if request.method == 'POST':
@@ -138,3 +139,53 @@ def userUpdate(request, id):
             'delta24': delta24,
             'id': id,
         })
+
+
+def userUpdateSubmit(request, id):
+    user = request.user
+    times = [
+        "3 PM", "3:30 PM", "4 PM", "4:30 PM", "5 PM",
+        "5:30 PM", "6 PM", "6:30 PM", "7 PM", "7:30 PM"
+    ]
+    today = datetime.now()
+    minDate = today.strftime('%Y-%m-%d')
+    deltatime = today + timedelta(days=21)
+    strdeltatime = deltatime.strftime('%Y-%m-%d')
+    maxDate = strdeltatime
+
+    day = request.session.get('day')
+    service = request.session.get('service')
+
+    # Only show the time of the day that has not been selected before and the time he is editing:
+    hour = checkEditTime(times, day, id)
+    appointment = Appointment.objects.get(pk=id)
+    userSelectedTime = appointment.time
+    if request.method == 'POST':
+        time = request.POST.get("time")
+        date = dayToWeekday(day)
+
+        if service is not None:
+            if day <= maxDate and day >= minDate:
+                if Appointment.objects.filter(day=day, time=time).count() < 1 or userSelectedTime == time:
+                    AppointmentForm = Appointment.objects.filter(pk=id).update(
+                        user=user,
+                        service=service,
+                        day=day,
+                        time=time,
+                    )
+                    messages.success(request, "Appointment Edited!")
+                    return redirect('index')
+                else:
+                    messages.success(
+                        request, "The Selected Time Has Been Reserved Before!")
+            else:
+                messages.success(
+                    request, "The Selected Date Isn't In The Correct Time Period!")
+        else:
+            messages.success(request, "Please Select A Service!")
+        return redirect('userPanel')
+
+    return render(request, 'userUpdateSubmit.html', {
+        'times': hour,
+        'id': id,
+    })
