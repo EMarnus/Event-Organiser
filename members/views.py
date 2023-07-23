ca970045e1174323948f1fb3b44a547d44279122
Emile <bvcxzq1@gmail.com>
2023-07-23 21:03:05 +0000

    Formating and Login Error message

--- members/views.py
+++ members/views.py
@@ -10,10 +10,9 @@ def login_user(request):
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			login(request, user)
-			return redirect('home')
+			return redirect('index')
 		else:
 			messages.success(request, ("There Was An Error Logging In, Try Again..."))
 			return redirect('login')
-
 	else:
 		return render(request, 'authenticate/login.html', {})
