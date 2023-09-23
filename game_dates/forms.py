ac14be48acf8dca56da254128ee90043d2beea25
Emile <bvcxzq1@gmail.com>
2023-09-23 14:49:32 +0000

    Change style for BookingDetails Lower section

--- game_dates/forms.py
+++ game_dates/forms.py
@@ -6,3 +6,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        }
+
+        labels = {
+            'body': ''
+        }
